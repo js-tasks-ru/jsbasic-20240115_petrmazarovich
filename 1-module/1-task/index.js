d6da21a94a14c531e6c0aa17896398cd66260ff7
Peter Mazarovich <petrmazarovich@gmail.com>
2024-01-23 11:01:54 +0300

    fixup! Basics, Objects

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -1,8 +1,10 @@
 function factorial(n) {
   let result = 1;
-  do {
-    result = result * n;
-    n = n - 1;
-  } while (n !== 1);
+  if (n > 0) {
+    do {
+      result = result * n;
+      n = n - 1;
+    } while (n >= 1);
+  }
   return result;
 }
