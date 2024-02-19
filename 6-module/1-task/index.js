94aaa6d9af63ce6272e1f546c8cde18853b24765
Peter Mazarovich <petrmazarovich@gmail.com>
2024-02-20 01:16:50 +0300

    Classes and components

--- 6-module/1-task/index.js
+++ 6-module/1-task/index.js
@@ -12,7 +12,27 @@
  *      }
  *
  */
+
+
 export default class UserTable {
+  #appendRow(obj, tableElement) {
+    let row = tableElement.insertRow(); // <tr>
+    Object.values(obj).forEach((value) => {
+      let cell = row.insertCell();
+      cell.innerText = value;
+    });
+    let cell = row.insertCell(); // <td>
+    let button = document.createElement("button");
+    button.innerText = "X";
+    cell.appendChild(button);
+
+    button.onclick = (event) => {
+      row.remove();
+    };
+  }
   constructor(rows) {
+    // в конструкторе и сформируем таблицу, т.к. в конструкторе происходит инициализация всех компонентов класса
+    this.elem = document.createElement("table");
+    rows.forEach(obj => this.#appendRow(obj, this.elem));
   }
 }
